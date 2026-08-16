37917b78683121561035f4d4929bf9ee0de889c9
AtiyaSanjidahFaiza <atiyasanjida798@gmail.com>
2026-08-16 21:11:00 +0600

    Add files via upload

---
+++ module.py
@@ -0,0 +1,14 @@
+import calculation
+
+print(calculation.add(10, 20))
+print(calculation.multiply(10, 20))
+import random
+print(random.randint(1, 5))
+import sys
+for p in sys.path:
+    print(p)
+from math import *
+print(sqrt(16))
+print(factorial(6))
+import math as m
+print(m.pi)
